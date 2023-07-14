1613c0e9e3b95f811d3074f1f8cff689d242afda
Roman Zotov <mr_monkey@inbox.ru>
2023-07-14 20:17:29 +0300

    5hw oop is done

---
+++ src/Game/java/units/Archer.java
@@ -0,0 +1,9 @@
+package units;
+
+public class Archer extends Shooter{
+    public Archer(String name, int x, int y, int nT) {
+        super(14, name, new int[]{1,4},5,2, x, y, nT, 5, 6);
+    }
+
+
+}
---
+++ src/Game/java/units/Warrior.java
@@ -0,0 +1,11 @@
+package units;
+
+public abstract class Warrior extends Person{
+
+    public Warrior(float hp, String name, int[] damage, int attack, int def, int x, int y, int nT, int initiative) {
+
+        super(hp, name, damage, attack, def, x, y, nT, initiative);
+    }
+
+
+}
